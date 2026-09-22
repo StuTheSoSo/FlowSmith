1c5cddff0500116e397f20481baf2298a94ae549
Stu Schwartz <stu.schwartz@gmail.com>
2026-09-22 10:07:59 -0400

    ios watch app working

---
+++ ios/App/FlowSmith Watch Watch App/FlowSmith_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowSmith_WatchApp.swift
+//  FlowSmith Watch Watch App
+//
+//  Created by Stu Schwartz on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowSmith_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
